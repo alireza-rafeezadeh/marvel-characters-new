3fbb869c5aa424d79fe8880401c748fb10d120c4
Alireza Rafeezadeh <rafeezadeh.alireza@gmail.com>
2022-01-03 00:28:09 +0100

    Add comics list to detail page

--- hero/ui-heroDetail/src/main/java/com/disney/ui_heroDetail/HeroDetail.kt
+++ hero/ui-heroDetail/src/main/java/com/disney/ui_heroDetail/HeroDetail.kt
@@ -1,14 +1,21 @@
 package com.disney.ui_heroDetail
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
@@ -57,8 +64,37 @@ fun HeroDetail(id: Int, singleHero: MutableState<Hero?>, imageLoader: ImageLoade
             style = TextStyle(textAlign = TextAlign.Center, fontSize = 25.sp)
 
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Text(text = "Comics")
+        Spacer(modifier = Modifier.height(4.dp))
+
+        LazyRow() {
+            singleHero.value?.comics?.items?.let { list ->
+                items(list) { comic ->
+                    Box(
+                        modifier = Modifier
+                            .border(width = 1.dp, color = Color.Red)
+                            .fillMaxHeight(0.3f)
+                            .width(160.dp)
+
+//                        .height(45.dp)
+                    ) {
+                        Text(
+                            text = comic.name,
+                            modifier = Modifier.align(alignment = Alignment.Center),
+                            style = TextStyle(fontSize = 13.sp, textAlign = TextAlign.Center)
+
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                }
+            }
+        }
     }
 
-    singleHero.value?.name
-    //todo: pass state here and show
+
 }
