e9cf31e3e9355ba01ae24b77f72acc7b09c1765f
Alireza Rafeezadeh <rafeezadeh.alireza@gmail.com>
2022-01-17 11:37:55 +0100

    temp

--- hero/ui-heroList/src/main/java/com/disney/ui_heroList/ui/Heros.kt
+++ hero/ui-heroList/src/main/java/com/disney/ui_heroList/ui/Heros.kt
@@ -1,5 +1,6 @@
 package com.disney.ui_heroList.ui
 
+import android.graphics.Color.red
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -8,13 +9,21 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.ImageLoader
@@ -34,6 +43,9 @@ fun HeroList(
     imageLoader: ImageLoader,
     navigateToDetail : (Int) -> Unit
 ) {
+
+    var text by remember { mutableStateOf("Hello") }
+
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -46,6 +58,27 @@ fun HeroList(
 
             item {
                 Spacer(modifier = Modifier.height(8.dp))
+
+                TextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("AquaMan") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp)
+                ,shape = RoundedCornerShape(8.dp),
+                    trailingIcon = {
+                        Icon(Icons.Filled.Search, "", tint = colorResource(id = R.color.red))
+
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = colorResource(id = R.color.card_background),
+                    focusedIndicatorColor =  Color.Transparent, //hide the indicator
+                    unfocusedIndicatorColor = Color.Transparent)
+
+
+                )
+
             }
 
             items(heroes.value) {
@@ -87,7 +120,8 @@ fun HeroList(
                         Text(
                             text = it.description, color = colorResource(id = R.color.text_description),
 //                            modifier = Modifier.weight(2f),
-                            fontSize = 11.sp
+                            fontSize = 10.sp,
+                            maxLines = 3
                         )
 //                    Spacer(modifier = Modifier.height(32.dp))
                     }
@@ -95,8 +129,8 @@ fun HeroList(
                     Image(
                         modifier = Modifier
 //                            .width(200.dp)
-                            .weight(3f)
-                            .height(290.dp)
+                            .weight(1.4f)
+                            .height(100.dp)
                             .clip(RoundedCornerShape(20.dp))
                         ,
                         painter = painter,
