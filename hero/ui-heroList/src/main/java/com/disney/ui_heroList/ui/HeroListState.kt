8a0a7ca3ea54fe3c70547b4874cf515d3e0fc8e1
Alireza <AlirezaTtest1@test.com>
2022-01-23 10:00:12 +0100

    Add hero list state _SQ

---
+++ hero/ui-heroList/src/main/java/com/disney/ui_heroList/ui/HeroListState.kt
@@ -0,0 +1,9 @@
+package com.disney.ui_heroList.ui
+
+import com.disney.core.ProgressBarState
+import com.disney.hero_domain.Hero
+
+data class HeroListState(
+    val progressBarState: ProgressBarState = ProgressBarState.Idle,
+    val heroes: List<Hero> = listOf()
+)
