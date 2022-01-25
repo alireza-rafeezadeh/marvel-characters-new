3bc12d82a9dba2c98fbd797259d5587c47916005
Alireza <AlirezaTtest1@test.com>
2022-01-25 17:26:31 +0100

    Add get heroes malformed data test

--- hero/hero-interactors/src/test/java/com/disney/hero_interactors/GetHeroesTest.kt
+++ hero/hero-interactors/src/test/java/com/disney/hero_interactors/GetHeroesTest.kt
@@ -3,6 +3,7 @@ package com.disney.hero_interactors
 import com.disney.core.Data
 import com.disney.core.Loading
 import com.disney.core.ProgressBarState
+import com.disney.core.Response
 import com.disney.hero_datasource_test.network.HeroDataValid.NUM_HEROS
 import com.disney.hero_datasource_test.network.HeroServiceFake
 import com.disney.hero_datasource_test.network.HeroServiceResponseType
@@ -17,7 +18,7 @@ class GetHerosTest {
     private lateinit var getHeros: GetHeros
 
     @Test
-    fun getHeros_success() =  runBlocking {
+    fun `get heroes valid data should succeed`() =  runBlocking {
         // setup
 //        val heroDatabase = HeroDatabaseFake()
 //        val heroCache = HeroCacheFake(heroDatabase)
@@ -50,5 +51,30 @@ class GetHerosTest {
 
         // Confirm loading state is IDLE
 //        assert(emissions[2] == Loading<List<Hero>>(ProgressBarState.Idle))
+    }
+
+    @Test
+    fun `get heroes malformed data should fail`() = runBlocking {
+
+        // setup
+//        val heroDatabase = HeroDatabaseFake()
+//        val heroCache = HeroCacheFake(heroDatabase)
+        val heroService = HeroServiceFake.build(
+            type = HeroServiceResponseType.Malformed // good data
+        )
+
+        getHeros = GetHeros(
+//            cache = heroCache,
+            service = heroService
+        )
+
+        // Execute the use-case
+        val emissions = getHeros.execute().toList()
+
+        // First emission should be loading
+        assert(emissions[0] == Loading<List<Hero>>(ProgressBarState.Loading))
+
+        // Confirm second emission is data
+        assert(emissions[1] is Response)
     }
 }
